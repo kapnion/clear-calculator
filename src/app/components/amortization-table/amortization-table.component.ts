81f0ea0567e9abee179fe440cacc45dd48bcb649
kapnion <karsten.beuerle@gmail.com>
2025-04-10 21:31:36 +0200

    feat: Scaffold basic structure
    feat: Scaffold basic structure

---
+++ src/app/components/amortization-table/amortization-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-amortization-table',
+  imports: [],
+  templateUrl: './amortization-table.component.html',
+  styleUrl: './amortization-table.component.scss'
+})
+export class AmortizationTableComponent {
+
+}
---
+++ src/app/models/summary-data-model.ts
@@ -0,0 +1,2 @@
+export interface SummaryDataModel {
+}
